86dfd15788bf31efb8a54f9f33d348ee40205458
Chonny <mama.whowho@gmail.com>
2021-08-06 16:08:18 +0800

    chore: add tailwindcss

---
+++ template/tailwind.config.js
@@ -0,0 +1,14 @@
+const path = require('path')
+
+module.exports = {
+  important: true,
+  darkMode: false,
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
